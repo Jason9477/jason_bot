3dd086d665d293013b8eccf9a19e53397d57eb81
Jason <allen0981335260@gmail.com>
2021-07-05 10:30:36 +0800

    first commit

---
+++ src/news.py
@@ -0,0 +1,139 @@
+# 檔名：news.py
+# 功能：印出各個國家的人口數據
+
+
+import discord
+from discord.ext import commands
+# import asyncio
+import requests
+from bs4 import BeautifulSoup
+
+
+class News(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(
+        help = '''
+            查看即時新聞from tvbs
+         ''',
+        brief = ":Print latest news from tvbs"
+    )
+    async def tvbs(self, ctx):
+        news_type=['politics','local','world','health','entertainment','life','money','tech','travel','sports']
+        await ctx.send("想要查看什麼類型的新聞")
+        await ctx.send("1.要聞\n2.社會\n3.全球\n4.健康\n5.娛樂\n6.生活\n7.理財房地產\n8.科技\n9.食尚\n10.運動")
+        
+        flag=True
+        while flag==True:
+            flag=False
+            try:
+                ans=await self.bot.wait_for('message',timeout=300.0)
+            except:
+                await ctx.send("輸入錯誤，請重新輸入1-10")
+                flag=True
+            if flag==False: 
+                if int(ans.content)<=10 and int(ans.content)>=1:
+                    r=requests.get("https://news.tvbs.com.tw/realtime/"+news_type[int(ans.content)-1])
+                    soup=BeautifulSoup(r.text,"html.parser")
+                    results=[]
+                    re=[]
+                    k=0
+                    for _ in [1,2,3,5,6,7,8,10,11,12]:
+                        re.append(soup.select("body > div.container > main > div > article > div.news_list > div.list > ul > li:nth-child("+str(_)+")>a")[0].attrs['href'])
+                        results.append(soup.select("body > div.container > main > div > article > div.news_list > div.list > ul > li:nth-child("+str(_)+") > a > h2")[0].text)
+                        await ctx.send(str(k+1)+"."+results[k].replace("\\u3000"," "))
+                        k+=1
+                        
+                    await ctx.send("輸入數字查看完整報導,可連續q或quit離開")
+                    flag3=True
+                    while flag3==True:
+                        ans2=await self.bot.wait_for('message',timeout=300.0)
+                        try:
+                            if int(ans2.content)<=10 and int(ans2.content)>=1:
+                                await ctx.send("https://news.tvbs.com.tw"+re[int(ans2.content)-1])
+                            else:
+                                await ctx.send("input error")
+                        except:
+                            if str(ans2.content)=='q' or str(ans2.content)=='quit':
+                                await ctx.send("quit")
+                                flag3=False
+                else:
+                    await ctx.send("輸入錯誤，請輸入1-10")
+                    flag=True
+        
+    @commands.command(help = "查看即時新聞from yahoo",brief = ":Print latest news from yahoo")
+    async def yahoo(self, ctx):
+
+        news_type=["","entertainment","world","politics","society","finance","sports","lifestyle","technology","health"]
+        await ctx.send("想要查看什麼類型的新聞")
+        await ctx.send("1.焦點\n2.娛樂\n3.國際\n4.政治\n5.社會地方\n6.財經\n7.體育\n8.生活\n9.科技\n10.健康")
+
+                
+        flag=True
+        while flag==True:
+            flag=False
+            try:
+                answer=await self.bot.wait_for('message',timeout=300.0)
+                
+            except:
+                await ctx.send("輸入錯誤，請重新輸入1-10")
+                flag=True
+            if flag==False:
+                try:
+                    ans10=int(answer.content)
+                except:
+                    await ctx.send("quit!!")
+                if int(answer.content)<=10 and int(answer.content)>=1:
+                    r=requests.get("https://tw.news.yahoo.com/"+news_type[int(answer.content)-1])
+                    soup=BeautifulSoup(r.text,"html.parser")
+                    results=[]
+                    re=[]
+                    k=0
+                    a=0
+                    n=0
+                    for _ in range(1,100):
+                        try:
+                            re.append(soup.select("li.js-stream-content:nth-child("+str(_)+") > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h3:nth-child(2) > a:nth-child(1)")[0].attrs['href'])
+                            results.append(soup.select("li.js-stream-content:nth-child("+str(_)+") > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h3:nth-child(2) > a:nth-child(1)")[0].text)
+                            await ctx.send(str(k+1)+"."+results[k])
+                            k+=1
+                        except:
+
+                            k=k
+                        if k==10:
+                            break
+                    await ctx.send("輸入數字查看完整報導，可連續q或quit離開")
+                    flag2=True
+                    while flag2==True:
+                        
+                        ans3=await self.bot.wait_for('message',timeout=300.0)
+                        try:
+                            if int(ans3.content)<=10 and int(ans3.content)>=1:
+                                await ctx.send("https://tw.news.yahoo.com"+re[int(ans3.content)-1])
+                                flag2=True
+                                
+                            else:
+                                await ctx.send("quit")
+                                
+                        except:
+                            if str(ans3.content)=='q' or str(ans3.content)=='quit':
+                                await ctx.send("quit")
+                                flag2=False
+                            else:
+                                await ctx.send("input error")
+                        
+
+                else:
+                    await ctx.send("輸入錯誤，請輸入1-10")
+                    flag=True
+
+                    
+                    
+                
+                
+            
+    
+       
+def setup(bot):
+    bot.add_cog(News(bot))
---
+++ src/population.py
@@ -0,0 +1,127 @@
+# 檔名：population.py
+# 功能：印出各個國家的人口數據
+
+from unittest import result
+import discord
+from discord.ext import commands
+# import asyncio
+import requests
+from bs4 import BeautifulSoup
+import time
+
+class Population(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(
+        help = '''
+
+            印出各個國家的人口數據
+
+            $population
+            等待
+            
+            輸入一或多個數字並以逗號分隔
+
+         ''',
+        brief = "Print world's population statics"
+    )
+    async def population(self, ctx):
+        await ctx.send("選擇下面一或多個國家或是輸入world查看整個世界(資料可能有延遲）")
+        e=discord.Embed()
+        e.set_image(url='https://raw.githubusercontent.com/Jason9477/7/master/New%20Project.png')
+        await ctx.send(embed=e)
+        country=await self.bot.wait_for('message',timeout=300.0)
+        c_list=[]
+        input_list=country.content.split(',')
+        print(input_list)
+        for _ in range(len(input_list)):
+
+            try:
+                if int(input_list[_])>50 or int(input_list[_])<1:
+                    await ctx.send('input error')
+                    return 
+                else:
+                    with open('../storage/country_list.txt','r') as f: 
+                        n=f.readlines()[int(input_list[_])-1].split('\n')[0]
+
+                    url='https://countrymeters.info/cn/'+n
+            except:
+                if input_list[_]=='world':
+                    n='world'
+                    url='https://countrymeters.info/cn/world'
+                else:
+                    await ctx.send('input error')
+                    return 
+            c_list.append(n)
+            r=requests.get(url)
+            print(url)
+            soup=BeautifulSoup(r.text,"html.parser")
+
+            results=soup.select("#cp1")
+            await ctx.send(n+'\'s static')
+
+
+            text = [[]]*len(input_list)
+            data=['目前人口','目前男性人口','目前女性人口','目前女性人口','目前女性人口','今年出生人數','今天出生人數','今年死亡人數','今天死亡人數','今年淨遷移','今天淨遷移','今年人口增長','今天人口增長']
+
+            for k in [i for i in range(13) if i!=3 and i!=4]:
+                
+                results=soup.find(id="cp"+str(k+1)).text
+                results=str(results).replace(' ',',')
+                text[_].append(f'{data[k]}: {results}人')
+
+            await ctx.send('\n'.join(text[_]))
+            if len(input_list)!=1 and _!=len(input_list)-1:
+                await ctx.send('==========================')
+        flag=True
+        while flag==True:
+            flag=False
+            await ctx.send('要將查詢資料存下來嗎[y/n]')
+            yn=await self.bot.wait_for('message',timeout=300.0)
+            if yn.content=='y' or yn.content=='yes':
+                localtime = time.asctime( time.localtime(time.time()) )
+                with open('../storage/output.txt', 'wt') as f:
+                    f.write(localtime+'\n')
+                    for i in range(len(input_list)):
+                        f.write(c_list[i]+'\'s static')
+                        print(c_list[i])
+                        for _ in range(11):
+                            f.write('\n'+text[i][_])
+                        if i!=len(input_list)-1:
+                            f.write('\n=============================\n')
+                
+                await ctx.send(file=discord.File('../storage/output.txt'))
+                return
+            elif yn.content=='n' or yn.content=='no':
+                await ctx.send("ok")
+
+                return
+            else:
+                flag=True
+    @commands.command(help='''印出人口數排名前幾名國家數據
+                              $toppop [number] 
+                              1<=[number]<=20''',brief="Print top population country's statics")
+    async def toppop(self,ctx,num):
+        try:
+            if int(num)<=20 and int(num)>=1:
+                    r=requests.get("https://countrymeters.info/cn")
+                    soup=BeautifulSoup(r.text,"html.parser")
+
+                    results=soup.select("div.review:nth-child(5) > table:nth-child(2)")[0].find_all("td")
+                    await ctx.send("country".ljust(40)+"population".center(40)+"rate".rjust(40))
+                    for _ in range(int(num)):
+                        result1=results[5*_+2].find("a").attrs['href'].split("/")[-1]
+                        result2=results[5*_+3].text.replace(" ",",")
+                        result3=results[5*_+4].text
+                        await ctx.send(result1.ljust(50) + result2.center(50)+result3.rjust(50))
+            else:
+ 
+                await ctx.send("number out of range")
+        except:
+            await ctx.send("input error")
+        
+ 
+
+def setup(bot):
+    bot.add_cog(Population(bot))
