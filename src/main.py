0f5e08cbf582c9ebbfb348a6a79d24a3035f2c98
Jason9477 <55344688+Jason9477@users.noreply.github.com>
2021-09-22 16:54:43 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -36,12 +36,7 @@ async def on_message(message):
     # 檢查訊息是否是 bot 自己傳的
     if message.author.id == bot.user.id:
         return
-    if message.author.id==703474678672392282:
-        for i in range(1,100):
-            await message.channel.send("閉嘴")
-        
-        
-        return
+    
     if message.content.lower().isdigit() and len(message.content.lower())==4 and message.author.id==690742477086261266:
         await message.channel.send("答對囉")
         return
